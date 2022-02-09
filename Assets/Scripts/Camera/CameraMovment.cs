514d2df02e70547c20fb7d28788e3c96981e23ac
almirph <phelipear@gmail.com>
2022-02-09 13:58:46 -0300

    camera movement

---
+++ Assets/Scripts/Camera/CameraMovment.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovment : MonoBehaviour
+{
+    [SerializeField] private float movmentSize;
+    [SerializeField] private float scrollMovmentSize;
+    void Update()
+    {
+        Movment();
+    }
+
+    void Movment()
+    {
+        //Arrow up and down
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position =  new Vector3(transform.position.x, transform.position.y, transform.position.z - movmentSize);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + movmentSize);
+        }
+
+        //Arrow left and right
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(transform.position.x - movmentSize, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector3(transform.position.x + movmentSize, transform.position.y, transform.position.z);
+        }
+
+        //Mouse scroll up and down
+        if (Input.mouseScrollDelta.y > 0f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - scrollMovmentSize, transform.position.z);
+        }
+
+        if (Input.mouseScrollDelta.y < 0f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + scrollMovmentSize, transform.position.z);
+        }
+    }
+}
